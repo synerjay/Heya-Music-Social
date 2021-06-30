b49aa0b98cc4913ed214898fd68c0fa35fe0b512
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-30 20:58:21 +0900

    added post and get request for post on the same url

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -15,15 +15,47 @@ Users = apps.get_model('users', 'CustomUser')
 
 # Create your views here.
 
-# GET request to view ALL posts made by all users (Feed)
-@api_view(["GET"])
+# // @router  GET api/posts 
+#             POST /posts
+# // @desc    Get all posts
+              # Create a post
+# // @access  Private
+@api_view(["GET", "POST"])
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def get_all_posts(request):
-    posts = Post.objects.all() # Get all posts
-    serializer = PostSerializer(posts, many=True)
-    # PROCESSING DATA IN WHICH FRONTEND CAN READ
-    data = serializer.data
-    for item in data:
-        item["added_by"] = Users.objects.get(id=item["added_by"]).username
-    return JsonResponse({'books': data }, safe=False, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        posts = Post.objects.all() # Get all posts
+        serializer = PostSerializer(posts, many=True)
+        # PROCESSING DATA IN WHICH FRONTEND CAN READ
+        data = serializer.data
+        for item in data:
+            item["added_by"] = Users.objects.get(id=item["added_by"]).username
+        return JsonResponse({'books': data }, safe=False, status=status.HTTP_200_OK)
+    else:
+        pass
+        # Put POST request logic here
+
+# // @router  GET /posts/:id
+# // @desc    Get one post by ID
+# // @access  Private
+
+# // @router  DELETE /posts/:id
+# // @desc    Delete a post
+# // @access  Private
+
+# // @router  PUT /posts/like/:id
+# // @desc    Like a post
+# // @access  Private
+
+# // @router  PUT /posts/unlike/:id
+# // @desc    Unlike a post
+# // @access  Private
+
+# // @router  Posting COMMENTS - POST /posts/comment/:post_id
+# // @desc    Comment on a post
+# // @access  Private
+
+# // @router  DELETE /posts/comment/:postid/:comment_id
+# // @desc    Comment on a post
+# // @access  Private
