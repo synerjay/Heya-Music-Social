b522c5bda21c0f238ff558e72b4673fbbdb82144
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-04 15:36:50 +0900

    added notes for routes for artists genre tracks

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -25,7 +25,7 @@ Users = apps.get_model('users', 'CustomUser')
 def get_all_profiles(request):
     profiles = Profile.objects.all()
     serializer = ProfileSerializer(profiles, many=True)
-    return JsonResponse({'books': serializer.data }, safe=False, status=status.HTTP_200_OK)
+    return JsonResponse({'profiles': serializer.data }, safe=False, status=status.HTTP_200_OK)
 
 # // @route GET profile/member/<int:user_id>
 # // @desc Get profile by user ID using params
@@ -78,15 +78,39 @@ def create_delete_profile(request):
             serializer = ProfileSerializer(profile)
             data = serializer.data
             data["user"] = Users.objects.get(id=user.id).username
-            return JsonResponse({'post': data}, safe=False, status=status.HTTP_200_OK)
+            return JsonResponse({'profile': data}, safe=False, status=status.HTTP_200_OK)
         except Profile.DoesNotExist:
             profile = Profile.objects.create(user=user, bio=payload["bio"]) # add more fields in the future
             serializer = ProfileSerializer(profile)
             data = serializer.data
             data["user"] = Users.objects.get(id=user.id).username
-            return JsonResponse({'post': data}, safe=False, status=status.HTTP_201_CREATED)
+            return JsonResponse({'profile': data}, safe=False, status=status.HTTP_201_CREATED)
         except Exception:
                 return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     elif request.method == 'DELETE':
         user.delete()
         return JsonResponse({'Success': 'User account deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+# // @route PUT /profile/genre
+# // @desc Add profile genre
+# // @access Private
+
+# // @route DELETE /profile/genre/<int:gen_id>
+# // @desc Delete genre from profile
+# // @access Private
+
+# // @route PUT /profile/artist
+# // @desc Add profile favorite artists
+# // @access Private
+
+# // @route DELETE /profile/artist/<int:art_id>
+# // @desc Delete artists from profile
+# // @access Private
+
+# // @route PUT /profile/tracks
+# // @desc Add profile favorite tracks
+# // @access Private
+
+# // @route DELETE /profile/artist/<int:track_id>
+# // @desc Delete tracks from profile
+# // @access Private
