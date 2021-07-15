9c25c92d69a3c595483b68fc7e75b295b145926a
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-15 19:04:26 +0900

    Changed the serializer file to include comments on the post

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Post, Comment 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'body', 'added_by', 'date_added', 'likes']
-
-# Needs to be edited !!
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'post', 'added_by', 'body', 'date_added']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, required=False) 
+
+    class Meta:
+        model = Post
+        fields = ['id', 'body', 'added_by', 'date_added', 'likes', 'comments']
+
+
