9674805fb37d3d9cce7d5d2d747292fbc2093a7c
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-16 21:16:52 +0900

    added models

--- backend/updates/admin.py
+++ backend/updates/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Update, Message
 
 # Register your models here.
+
+admin.site.register(Update)
+admin.site.register(Message)
--- backend/updates/models.py
+++ backend/updates/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+from profiles.models import Profile
 
 # Create your models here.
+
+class Update(models.Model):
+    body = models.TextField()
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None, related_name='updates')
+    date_added = models.DateTimeField(auto_now_add=True) # adds the date to now 
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='updates', blank=True) # many 
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.body[:50] + '...' + str(self.added_by)
+
+class Message(models.Model):
+    update = models.ForeignKey(Update, on_delete=models.CASCADE, related_name='messages')
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None, related_name='messages')
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.body[:50], self.added_by)
