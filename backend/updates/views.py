67ec68baef2117d5f1820914c1bba00ffcb95e9f
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-18 16:13:25 +0900

    Added message deletion capability

--- backend/updates/views.py
+++ backend/updates/views.py
@@ -16,35 +16,41 @@ Profile = apps.get_model('profiles', 'Profile')
 
 
 # Create your views here.
-# // @router  GET, POST /posts 
+# // @router  GET, POST /updates
 # // @desc    Get all posts, Create a post
 # // @access  Private
 @api_view(["GET", "POST"])
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def get_add_updates(request):
-    # if request.method == 'GET':
-    #     posts = Post.objects.all() # Get all posts
-    #     serializer = PostSerializer(posts, many=True)
-    #     # PROCESSING DATA IN WHICH FRONTEND CAN READ # try to make the post seralizer output the avatar url s
-    #     data = serializer.data
-    #     for item in data:
-    #         item["added_by"] = Users.objects.get(id=item["added_by"]).username
-    #     return JsonResponse({'posts': data }, safe=False, status=status.HTTP_200_OK)
-    # else:
-    payload = json.loads(request.body)
-    member = Users.objects.get(id=request.user.id)
-        # try:
-    profile = Profile.objects.get(user=request.user.id)
-    update = Update.objects.create(body=payload["body"], added_by=member, profile=profile)
-    serializer = UpdateSerializer(update, context={"request": request})
-    data = serializer.data
-    data["added_by"] = member.username 
-    return JsonResponse({'post': data}, safe=False, status=status.HTTP_201_CREATED)
-        # except ObjectDoesNotExist as e:
-        #     return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
-        # except Exception:
-        #     return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    if request.method == 'GET':
+        updates = Update.objects.all() # Get all posts
+        serializer = UpdateSerializer(updates, many=True, context={"request": request})
+        # PROCESSING DATA IN WHICH FRONTEND CAN READ # try to make the post seralizer output the avatar url s
+        data = serializer.data
+        for item in data:
+            item["added_by"] = Users.objects.get(id=item["added_by"]).username
+            if len(item["likes"]) != 0:
+                for i, like in enumerate(item["likes"]):
+                    item["likes"][i] = Users.objects.get(id=like).username
+            if len(item["messages"]) != 0:
+                for message in item["messages"]:
+                    message["added_by"] = Users.objects.get(id=message["added_by"]).username
+        return JsonResponse({ 'posts': data }, safe=False, status=status.HTTP_200_OK)
+    else:
+        payload = json.loads(request.body)
+        member = Users.objects.get(id=request.user.id)
+        try:
+              profile = Profile.objects.get(user=request.user.id)
+              update = Update.objects.create(body=payload["body"], added_by=member, profile=profile)
+              serializer = UpdateSerializer(update, context={"request": request})
+              data = serializer.data
+              data["added_by"] = member.username 
+              return JsonResponse({'post': data}, safe=False, status=status.HTTP_201_CREATED)
+        except ObjectDoesNotExist as e:
+            return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # // @router  GET one post, PUT, DELETE /updates/:id
 # // @desc    Get one post by ID, Update Post, Delete Post
@@ -55,11 +61,15 @@ def get_add_updates(request):
 def get_put_delete_update(request, update_id):
     if request.method == 'GET':
         try:
-            user = request.user
+            # user = request.user
             update = Update.objects.get(id=update_id)
             serializer = UpdateSerializer(update, context={"request": request})
             data = serializer.data
             data["added_by"] = Users.objects.get(id=data["added_by"]).username
+            for i, like in enumerate(data["likes"]):
+                    data["likes"][i] = Users.objects.get(id=like).username
+            for message in data["messages"]:
+                    message["added_by"] = Users.objects.get(id=message["added_by"]).username
             return JsonResponse({'posts': data }, safe=False, status=status.HTTP_200_OK)
         except ObjectDoesNotExist as e:
             return JsonResponse({'error': "Sorry, this post doesn't exist. "}, safe=False, status=status.HTTP_404_NOT_FOUND)
@@ -134,11 +144,20 @@ def create_message(request, update_id):
 @csrf_exempt
 @permission_classes([IsAuthenticated])
 def delete_message(request, update_id, message_id):
+    user = request.user
     update = Update.objects.get(id=update_id)
-    message = update.messages.get(id=message_id)
-    message.delete()
-    serializer = UpdateSerializer(update, context={"request": request})
-    data = serializer.data
-    for item in data["messages"]:
-        item["user"] = Users.objects.get(id=item["user"]).username
-    return JsonResponse({'Success': 'message deleted successfully', 'post': data}, safe=False, status=status.HTTP_201_CREATED)
+    try:
+        message = update.messages.get(id=message_id, added_by=user)
+        message.delete()
+        serializer = UpdateSerializer(update, context={"request": request})
+        data = serializer.data
+        data["added_by"] = Users.objects.get(id=data["added_by"]).username
+        for i, like in enumerate(data["likes"]):
+            data["likes"][i] = Users.objects.get(id=like).username
+        for message in data["messages"]:
+            message["added_by"] = Users.objects.get(id=message["added_by"]).username
+        return JsonResponse({'Success': 'message deleted successfully', 'post': data}, safe=False, status=status.HTTP_201_CREATED)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
