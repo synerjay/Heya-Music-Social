65ed8483246ada15000f2c0dc08fe9454ebb63dd
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-18 14:32:18 +0900

    Added messages capabilities

--- backend/updates/urls.py
+++ backend/updates/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.get_add_updates),
   # path('like/<int:update_id>', views.put_like),
-  # path('message/<int:update_id>', views.get_update_message),
-  # path('message/<int:update_id>/<int:message_id>', views.delete_message),
-  # path('<int:update_id>', views.get_put_delete_update),
+  path('message/<int:update_id>', views.create_message),
+  path('message/<int:update_id>/<int:message_id>', views.delete_message),
+  path('<int:update_id>', views.get_put_delete_update),
 ]
--- backend/updates/views.py
+++ backend/updates/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 #Auth dependencies
 from rest_framework.decorators import api_view, permission_classes #for authenticated routes
@@ -45,3 +45,112 @@ def get_add_updates(request):
         #     return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
         # except Exception:
         #     return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# // @router  GET one post, PUT, DELETE /updates/:id
+# // @desc    Get one post by ID, Update Post, Delete Post
+# // @access  Private
+@api_view(["GET", "DELETE", "PUT"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def get_put_delete_update(request, update_id):
+    if request.method == 'GET':
+        try:
+            user = request.user
+            update = Update.objects.get(id=update_id)
+            serializer = UpdateSerializer(update, context={"request": request})
+            data = serializer.data
+            data["added_by"] = Users.objects.get(id=data["added_by"]).username
+            return JsonResponse({'posts': data }, safe=False, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist as e:
+            return JsonResponse({'error': "Sorry, this post doesn't exist. "}, safe=False, status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    elif request.method == 'DELETE':
+        try:
+            user = request.user.id
+            update = Update.objects.get(user=user, id=update_id)
+            update.delete()
+            return JsonResponse({'Success': 'Post deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except ObjectDoesNotExist as e:
+            return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({'error': 'Something went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# // @router  PUT /updates/like/:id
+# // @desc    Like and Unlike a post
+# // @access  Private
+@api_view(["PUT"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def put_like(request, update_id):
+    user = request.user
+    update = get_object_or_404(Update, id=update_id)
+    if update.likes.filter(id=request.user.id).exists(): # user already exists in the like list
+        update.likes.remove(user)
+    else:
+        update.likes.add(user) # add the like to the like array in the update object
+    total_likes = update.total_likes()
+    serializer = UpdateSerializer(update, context={"request": request})
+    data = serializer.data
+    data["user"] = user.username 
+    print(data["likes"])
+    # like_list = data["likes"]
+    for i, item in enumerate(data["likes"]):
+        data["likes"][i] = Users.objects.get(id=item).username # always put the username!!
+    return JsonResponse({'post': data, 'total_likes': total_likes }, safe=False, status=status.HTTP_200_OK)
+    # Might change above for changes in the frontend REACT later
+
+# // @router  POST messages on an update  /updates/message/<int:update_id>
+# // @desc    POST messages on an update
+# // @access  Private
+@api_view(["GET", "POST"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def create_message(request, update_id):
+    # if request.method == 'GET':
+    #     # comments = Comment.objects.filter(update=update_id) # objects with one or more queries, it needs to be a FILTER and not a GET method
+    #     update = update.objects.get(id=update_id)
+    #     # serializer = CommentSerializer(comments, many=True)
+    #     # serializer = CommentSerializer(update.comments.get(id=1))
+    #     serializer = CommentSerializer(update.comments.all(), many=True) # you can just use update.comments.all() get to all comments from that particular update
+    #     # PROCESSING DATA IN WHICH FRONTEND CAN READ
+    #     data = serializer.data
+    #     for item in data:
+    #         item["user"] = Users.objects.get(id=item["user"]).username
+    #     return JsonResponse({'comments': data }, safe=False, status=status.HTTP_200_OK)
+    # elif request.method == 'update':
+
+    payload = json.loads(request.body)
+    user = Users.objects.get(id=request.user.id)
+    update = Update.objects.get(id=update_id)
+        # try:
+            # In the Comment object, for some weird effin reason Django needs to get the specific INSTANCE of the object NOT the Key like the update object. WEIRDO DJANGO UGH
+    profile = Profile.objects.get(user=user.id)
+    Message.objects.create(update=update, added_by=user, body=payload["body"], profile=profile)
+    # comments = Comment.objects.filter(post=post_id)
+    serializer = UpdateSerializer(update, context={"request": request})
+    data = serializer.data
+    for item in data["messages"]:
+        item["added_by"] = Users.objects.get(id=item["added_by"]).username
+    return JsonResponse({'post': data}, safe=False, status=status.HTTP_201_CREATED)
+        # except ObjectDoesNotExist as e:
+        #     return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+        # except Exception:
+        #     return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+# // @router  DELETE /posts/comment/<int:post_id>/<int:comment_id>
+# // @desc    Delete a comment on a post
+# // @access  Private
+@api_view(["DELETE"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def delete_message(request, update_id, message_id):
+    update = Update.objects.get(id=update_id)
+    message = update.messages.get(id=message_id)
+    message.delete()
+    serializer = UpdateSerializer(update, context={"request": request})
+    data = serializer.data
+    for item in data["messages"]:
+        item["user"] = Users.objects.get(id=item["user"]).username
+    return JsonResponse({'Success': 'message deleted successfully', 'post': data}, safe=False, status=status.HTTP_201_CREATED)
