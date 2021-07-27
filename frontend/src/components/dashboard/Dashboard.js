c5a7b8211cac4af07f105ffbdfada010e0597833
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-27 19:48:11 +0900

    added a custom modal

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -15,6 +15,7 @@ import ArtistRec from '../recommendations/ArtistRec';
 import GenreRec from '../recommendations/GenreRec';
 import TrackRec from '../recommendations/TrackRec';
 import SearchAlbum from '../search/SearchAlbum';
+import CustomModal from '../layout/CustomModal';
 
 const Dashboard = ({
   accessToken,
@@ -59,55 +60,11 @@ const Dashboard = ({
           </button> */}
 
           {showAlbumModal ? (
-            <>
-              <div className='h-full w-full justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-                <div className='h-5/6 w-5/6 overflow-y-autorelative my-6 mx-auto max-w-3xl'>
-                  {/*content*/}
-                  <div className=' h-full border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-                    {/*header*/}
-                    <div className='h-1/6 flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t'>
-                      <h3 className='text-3xl font-semibold'>
-                        Add your favorite albums
-                      </h3>
-                      <button
-                        className='p-1 ml-auto bg-transparent border-0 text-black float-right text-xl leading-none font-semibold outline-none focus:outline-none'
-                        onClick={() => setShowAlbumModal(false)}
-                      >
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          className='h-6 w-6'
-                          fill='none'
-                          viewBox='0 0 24 24'
-                          stroke='currentColor'
-                        >
-                          <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            strokeWidth={2}
-                            d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                    {/*body*/}
-                    <div className='h-full w-full relative p-6 flex-auto overflow-scroll'>
-                      <SearchAlbum />
-                    </div>
-                    {/*footer*/}
-                    <div className=' h-3.5 flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b'>
-                      <button
-                        className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                        type='button'
-                        onClick={() => setShowAlbumModal(false)}
-                      >
-                        Close
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
-            </>
+            <CustomModal
+              title='Add your favorite albums'
+              component={SearchAlbum}
+              setShowModal={setShowAlbumModal}
+            />
           ) : null}
 
           {/*  Testing Modal Purposes */}
