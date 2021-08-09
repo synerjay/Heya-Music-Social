3f107a50aa38199242c0628b48e4e205511eb48b
Jerome Tolentino <tolent.jay@gmail.com>
2021-08-09 12:02:38 +0900

    Programmatically added like function on post item

--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -26,6 +26,13 @@ const PostItem = ({
   },
   showActions,
 }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = (id) => {
+    addLike(id);
+    setLiked(!liked);
+  };
+
   return (
     <Fragment>
       <div class='py-1 md:my-7 sm:max-w-xl sm:mx-auto'>
@@ -42,13 +49,19 @@ const PostItem = ({
               </div>
             </div>
             <div class='flex flex-col h-16 '>
-              <p class=' text-gray-400 max-h-20 overflow-y-hidden text-md'>
+              <p class=' text-gray-200 max-h-20 overflow-y-hidden text-md'>
                 {body}
               </p>
             </div>
             <div class='flex items-center justify-between mx-1 mt-1 mb-1'>
               <div class='flex gap-5'>
-                <svg fill='#FFFFFF' height='16' viewBox='0 0 48 48' width='16'>
+                <svg
+                  onClick={() => handleLike(id)}
+                  fill={liked ? '#ff0000' : '#ffffff'}
+                  height='16'
+                  viewBox='0 0 48 48'
+                  width='16'
+                >
                   <path d='M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z'></path>
                 </svg>
                 <svg fill='#FFFFFF' height='16' viewBox='0 0 48 48' width='16'>
@@ -68,15 +81,21 @@ const PostItem = ({
                 </svg>
               </div>
             </div>
-            <div class='font-semibold text-xs mx-1 mt-2 mb-4'>
-              {likes.length} {likes.length === 1 ? <>like</> : <>likes</>} ·
-              Posted{' '}
-              {formatDistanceToNow(
-                new Date(moment.utc(date_added).local().format()),
-                {
-                  addSuffix: true,
-                }
-              )}
+            <div class='text-xs mx-1 mt-2 mb-4'>
+              <p className='font-semibold'>
+                {likes.length} {likes.length === 1 ? <>like</> : <>likes</>} ·{' '}
+                {messages.length}{' '}
+                {messages.length === 1 ? <>Comment</> : <>Comments</>}
+              </p>
+              <p className='text-gray-500'>
+                Posted{' '}
+                {formatDistanceToNow(
+                  new Date(moment.utc(date_added).local().format()),
+                  {
+                    addSuffix: true,
+                  }
+                )}
+              </p>
             </div>
           </div>
           <div class='h-48 overflow-visible w-1/2'>
